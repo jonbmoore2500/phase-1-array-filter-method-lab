b05511c75848b9c07706335e9965a3c5dcc2cf9e
Jonathan <jonathanmoorevla@gmail.com>
2022-06-22 22:44:17 -0500

    commit tree 23b323629c755cf85facc9611a5d9e03a00f207c

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(collection, name) {
+    const matchingArray = [];
+
+    for (const member of collection) {
+       // can I make the toLowerCase more universal and not only 
+       // versions in all lower case?
+        if (member === name || member === name.toLowerCase()) {
+             matchingArray.push(member)
+        };
+    }
+    return matchingArray;
+}
+
+//console.log(findMatching(drivers, 'Bobby'))
+
+
+function fuzzyMatch(collection, name) {
+    const matchingArrayFuzz = [];
+    
+    for (const member of collection) {
+        if (member[0] === name[0]) {
+            matchingArrayFuzz.push(member)
+        }
+    }
+    return matchingArrayFuzz;
+}
+
+const driversExpanded = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(collection, name) {
+    const matchingArrayExpanded = [];
+
+    for (const member of collection) {
+        if (member.name === name) {
+            matchingArrayExpanded.push(member)
+        }
+    }
+return matchingArrayExpanded;
+}
